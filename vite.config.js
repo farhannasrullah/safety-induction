d11eb14d39dcaf50920819660ec991b507113cc9
Farhan Nasrullah <farhannasrullah3@gmail.com>
2026-04-02 19:51:20 +0700

    no vite

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), // Plugin Tailwind CSS v4
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'Digital Safety Induction',
-        short_name: 'SafetyInduct',
-        description: 'Aplikasi edukasi dan persetujuan keselamatan kerja (K3)',
-        theme_color: '#facc15', // Warna kuning (sesuai tema UI kita)
-        background_color: '#f8fafc', // Warna background slate-50
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable' // Penting untuk perangkat Android
-          }
-        ]
-      }
-    })
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    //   manifest: {
+    //     name: 'Digital Safety Induction',
+    //     short_name: 'SafetyInduct',
+    //     description: 'Aplikasi edukasi dan persetujuan keselamatan kerja (K3)',
+    //     theme_color: '#facc15', // Warna kuning (sesuai tema UI kita)
+    //     background_color: '#f8fafc', // Warna background slate-50
+    //     display: 'standalone',
+    //     orientation: 'portrait',
+    //     icons: [
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png'
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any maskable' // Penting untuk perangkat Android
+    //       }
+    //     ]
+    //   }
+    // })
   ],
 })
